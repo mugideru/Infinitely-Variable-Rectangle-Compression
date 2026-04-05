c17ff24a25b87b8aab50756a95270b45c151af2a
mugideru <mugideru010439@gmail.com>
2026-04-05 20:42:57 +0900

    llvm

--- build.bat
+++ build.bat
@@ -3,7 +3,13 @@ set Z_DIR=.\zstd
 echo Compiling...
 
 :: コンパイルだけを実行し、結果を ERRORLEVEL で判定する
-cl ivr_codec.c %Z_DIR%\common\*.c %Z_DIR%\compress\*.c %Z_DIR%\decompress\*.c /O2 /MT /I. /I"%Z_DIR%" /I"%Z_DIR%\common" /I"%Z_DIR%\compress" /I"%Z_DIR%\decompress" /Fe:ivr_converter.exe /utf-8
+clang-cl ivr_codec.c %Z_DIR%\common\*.c %Z_DIR%\compress\*.c %Z_DIR%\decompress\*.c ^
+    /O3 /Ot /Oi /Gw /Gy /GL ^
+    -mllvm -inline-threshold=1000 ^
+    /arch:AVX2 ^
+    /MT /I. /I"%Z_DIR%" /I"%Z_DIR%\common" /I"%Z_DIR%\compress" /I"%Z_DIR%\decompress" ^
+    /Fe:ivr_converter.exe /utf-8 ^
+    /link /LTCG
 
 if %ERRORLEVEL% equ 0 (
     echo "Compilation Succeeded!"
